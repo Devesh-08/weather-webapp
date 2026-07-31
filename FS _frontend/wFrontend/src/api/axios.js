b6607a1dd025a8986d1c33634241a7cc47e9e215
Devesh <prajapatidevesh765@gmail.com>
2026-07-31 11:39:09 +0530

    changes

--- FS _frontend/wFrontend/src/api/axios.js
+++ FS _frontend/wFrontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_URL,
+    baseURL: "https://weather-webapp-1rdy.onrender.com/api",
     withCredentials: true,
 });
 
